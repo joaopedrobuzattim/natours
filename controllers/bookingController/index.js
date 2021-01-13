2f4e7850b3e98d3f130ccf27145526f198cd9506
Joao Pedro Buzatti Mendes <joao.buzattimendes@gmail.com>
2021-01-13 01:04:38 -0300

    Final

--- controllers/bookingController/index.js
+++ controllers/bookingController/index.js
@@ -62,12 +62,14 @@ exports.getCheckoutSession = catchAsync(  async(req,res,next)=>{
  const createBookingCheckout = async session =>{
 
     const tour = session.client_reference_id;
-    const user = (await User.findOne( { email: session.customer_email } )).id
+    const user = await User.findOne( { email: session.customer_email } )
+    console.log(session.customer_email)
+    console.log(user);
     const price = session.display_items[0].amount / 100;
 
     await Booking.create({
         tour,
-        user,
+        user: user.id,
         price
     })
  }
